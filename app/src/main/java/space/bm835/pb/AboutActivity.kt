b96014bde71001e987cf585dab7c52c2988c8cd4
BeM835 <turejskijleonid004@gmail.com>
2019-07-02 16:30:57 +1000

    about activity added; shortcuts added

---
+++ app/src/main/java/space/bm835/pb/AboutActivity.kt
@@ -0,0 +1,17 @@
+package space.bm835.pb
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+
+    override fun onClickUn(v : View){
+        //TODO uninstall button
+    }
+}
