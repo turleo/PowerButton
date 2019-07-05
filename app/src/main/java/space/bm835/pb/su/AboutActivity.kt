bb87b5bafb34e48882326d248ea3ccbf9e2a9ffe
BeM835 <turejskijleonid004@gmail.com>
2019-07-05 18:15:09 +1000

    version shows in about activity

--- app/src/main/java/space/bm835/pb/su/AboutActivity.kt
+++ app/src/main/java/space/bm835/pb/su/AboutActivity.kt
@@ -1,10 +1,12 @@
 package space.bm835.pb.su
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.TextView
 import java.io.DataOutputStream
 import java.io.IOException
 
@@ -14,6 +16,12 @@ class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val buildInfoText = findViewById<TextView>(R.id.textView2)
+
+        val manager = this.packageManager
+        val info = manager.getPackageInfo(this.packageName, PackageManager.GET_ACTIVITIES)
+        buildInfoText.text = info.versionName +'('+ info.versionCode +") SU branch"
     }
 
     fun onClickUn(v : View){
